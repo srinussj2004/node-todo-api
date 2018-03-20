aca071da185c7aa8699cc0091956e5d6c863376d
ARTHAM <sa458e@att.com>
2018-03-19 23:00:12 -0700

    added heroku mongodb configueration

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -47,8 +49,8 @@ Todo.findById(id).then((todo) => {
 });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
