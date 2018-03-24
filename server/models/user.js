a80865f23cddcadfacd3795aab9cd8d67c321859
ARTHAM <sa458e@att.com>
2018-03-24 11:24:54 -0700

    delete or logout token test script

--- server/models/user.js
+++ server/models/user.js
@@ -49,6 +49,15 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
@@ -83,7 +92,6 @@ UserSchema.statics.findByCredentials = function (email,password) {
     });
   });
 };
-
 
 UserSchema.pre('save', function (next){
   var user = this;
